f2dd30a98a73e781a565faf468788085fcf43185
greolus <greolus@gmail.com>
2021-07-18 11:46:31 +0700

    hell level only once

--- make.bat
+++ make.bat
@@ -1,12 +1,11 @@
-rm -r pythontex-files-* 
-rm multi_table.pytxcode 
-rm multi_table.pdf
-docker run -w /root -v %cd%:/root/ texlive/texlive xelatex  multi_table.tex
-docker run -w /root -v %cd%:/root/ texlive/texlive pythontex multi_table.tex
-docker run -w /root -v %cd%:/root/ texlive/texlive xelatex  multi_table.tex
-rm multi_table.aux
-rm multi_table.log
-rm multi_table.pytxcode
-rm -r pythontex-files-*
-rm texput.log
-start multi_table.pdf
+rm -r pythontex-files-* 2>NUL
+rm  multi_table.pytxcode  multi_table.pdf 2>NUL
+
+@docker run -w /root -v %cd%:/root/ texlive/texlive xelatex  multi_table.tex > NUL
+@docker run -w /root -v %cd%:/root/ texlive/texlive pythontex multi_table.tex
+@docker run -w /root -v %cd%:/root/ texlive/texlive xelatex  multi_table.tex
+
+rm multi_table.aux multi_table.log multi_table.pytxcode texput.log 2>NUL
+rm -r pythontex-files-* 2>NUL
+
+@start multi_table.pdf
